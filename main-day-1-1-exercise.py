2df209b8785673694c4dcc7406323147bc4748ba
Raju Chowdhury <59740648+raju-chowdhury1982@users.noreply.github.com>
2021-08-14 22:43:34 +0530

    Add files via upload

---
+++ main-day-1-1-exercise.py
@@ -0,0 +1,18 @@
+#Write your code below this line 👇
+
+print("\nDay 1 - Python Print Function")
+
+print("\nThe function is declared like this:")
+
+print('\nprint(\'what to print\')\n')
+
+
+print("Welcome Here\n" * 5)
+
+
+
+
+
+
+
+
